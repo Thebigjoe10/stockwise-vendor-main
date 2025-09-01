8f65852be05a9c962dc0efe712709762393bc994
Josy <jojosr001@gmail.comgit config --global user.email jojosr001@gmail.com>
2025-09-01 19:36:23 +0100

    Updated main vendor dashbard page

--- app/vendor/dashboard/page.tsx
+++ app/vendor/dashboard/page.tsx
@@ -1,18 +1,4 @@
 import React from "react";
-import { FaCheckCircle } from "react-icons/fa";
-import { IoIosCloseCircle } from "react-icons/io";
-import { SlEye } from "react-icons/sl";
-import { HiCurrencyRupee } from "react-icons/hi";
-import Link from "next/link";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-} from "@mui/material";
 import {
   calculateTotalOrders,
   getDashboardData,
@@ -22,6 +8,9 @@ import OrdersSummaryCard from "@/components/vendor/dashboard/OrdersSummaryCard";
 import ProductData from "@/components/vendor/dashboard/product.perfomance";
 import LowStockProducts from "@/components/vendor/dashboard/low-stock-products";
 import OutOfStockProducts from "@/components/vendor/dashboard/out-of-stock-products";
+import OrdersTable from "@/components/vendor/dashboard/OrdersTable";
+
+// Main Vendor Dashboard Page
 
 const VendorDashboardPage = async () => {
   const data = await getDashboardData().catch((err) => {
@@ -60,51 +49,8 @@ const VendorDashboardPage = async () => {
         />
       {/* Recent Orders */}
       <div className="my-6">
-        <div className="SecondaryTitleStyle">Recent Orders</div>
-        <div className="w-full overflow-x-auto">
-          <TableContainer component={Paper}>
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Total</TableCell>
-                  <TableCell>Payment</TableCell>
-                  <TableCell>View</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {Array.isArray(data?.orders) && data.orders.length > 0 ? (
-                  data.orders.map((order: any, index: number) => (
-                    <TableRow key={order._id || index}>
-                      <TableCell>{order?.user?.email || "N/A"}</TableCell>
-                      <TableCell>₹ {order.total}</TableCell>
-                      <TableCell>
-                        {order.isPaid ? (
-                          <FaCheckCircle size={20} color="green" />
-                        ) : (
-                          <IoIosCloseCircle size={22} color="red" />
-                        )}
-                      </TableCell>
-                      <TableCell>
-                        <Link href={`/order/${order._id}`}>
-                          <SlEye />
-                        </Link>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell colSpan={4} className="text-center text-gray-500">
-                      No recent orders found
-                    </TableCell>
-                  </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
+        <OrdersTable data={{ orders: data?.orders || [] }} />
       </div>
-
       {/* Product stats */}
       <ProductData />
       <LowStockProducts />
