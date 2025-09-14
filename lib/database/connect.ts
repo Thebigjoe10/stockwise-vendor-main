f8f4026bbcba595a1feffe127faafcdfcfcda5db
Josy <jojosr001@gmail.comgit config --global user.email jojosr001@gmail.com>
2025-09-14 14:46:09 +0100

    changes

--- lib/database/connect.ts
+++ lib/database/connect.ts
@@ -22,7 +22,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "Stockwise",
+      dbName: "Stockwise/Tradelynk",
       bufferCommands: false,
     });
   cached.conn = await cached.promise;
